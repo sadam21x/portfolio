971413d013dc6b47f7597d9161775cc53020d984
Sadam <sadam21x@gmail.com>
2023-10-02 18:12:05 +0700

    chore: initialize project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio
+This is my personal website, an online space where I can share my experience, some thoughts, snippets, etc. Feel free to contribute or fork this project.
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.{tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        rubik: ['Rubik', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: ["light"]
+  },
+}
+export default config
