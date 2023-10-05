df50efac9dcfa7dd49ef0b8da079c8a0a01622df
Sadam <sadam21x@gmail.com>
2023-10-05 15:33:43 +0700

    chore: implement nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,19 @@
 import type { AppProps } from 'next/app'
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import Router from 'next/router'
-import Progress from '@/components/Progress'
+import nProgress from 'nprogress'
 import '@/styles/globals.css'
 import '@/styles/scrollbar.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(false)
-
   useEffect(() => {
-    Router.events.on('routeChangeStart', () => setLoading(true))
-    Router.events.on('routeChangeComplete', () => setLoading(false))
-    Router.events.on('routeChangeError', () => setLoading(false))
+    Router.events.on('routeChangeStart', () => nProgress.start())
+    Router.events.on('routeChangeComplete', () => nProgress.done())
+    Router.events.on('routeChangeError', () => nProgress.done())
   }, [])
 
   return (
     <div className='bg-[#F5F5F5]'>
-      <Progress loading={loading} />
       <Component {...pageProps} />
     </div>
   )
